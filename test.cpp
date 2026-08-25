e9ab9a2cdae024196e0135884dfe6de6b02b42bb
randomlyiii <2313346840@qq.com>
2026-08-25 10:11:02 +0800

    file:更新练习文件与笔记

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
-    long double pi = 3.1415926;
-    std::cout << std::fixed << std::setprecision(2) << "pi = " << pi << std::endl;
+    int num[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int (*p)[4];
+    p = (int (*)[4])num;
+    cout << (*(p + 1))[1] << endl; // 6
+    printf("%zu\n", sizeof(p));    // 8 因为p是指针，64位系统8个字节
+    printf("%p\n", p);
+    printf("%p\n", p + 1);
 }
